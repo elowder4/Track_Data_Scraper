41bdf7126686c7c8911a88ed4e82b6362099f5c4
Ethan Lowder <elowder@iwu.edu>
2024-10-19 13:15:17 -0500

    got multiple lists working

--- scraper.py
+++ scraper.py
@@ -14,11 +14,11 @@ import pandas as pd
 
 
 def scrape(username: str, password: str, lists: [str]) -> str:
-    '''Scrapes nscasports webpage by logging in with a given username and password.
+    """Scrapes nscasports webpage by logging in with a given username and password.
     username: username for login.
     password: password for login.
     lists: lists to loop over.
-    output: returns string sucess or failure'''
+    output: returns string success or failure"""
 
     # Store athlete data
     data = []
@@ -26,30 +26,29 @@ def scrape(username: str, password: str, lists: [str]) -> str:
     # Set URL
     url = 'https://coach-x.ncsasports.org/'
 
+    # Load environment variables
+    load_dotenv()
+
+    # Set Timout and Wait Time
+    timeout = 20
+
+    # Set Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.headless = True
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    # Use a random user agent
+    ua = UserAgent().random
+    chrome_options.add_argument(f'user-agent={ua}')
+
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
     try:
-        # Load environment variables
-        load_dotenv()
-
-        # Set Timout and Wait Time
-        timeout = 10
-
-        # Set Chrome options
-        chrome_options = Options()
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.headless = True
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        prefs = {"profile.managed_default_content_settings.images": 2}
-        chrome_options.add_experimental_option("prefs", prefs)
-
-        # Use a random user agent
-        ua = UserAgent()
-        userAgent = ua.random
-        chrome_options.add_argument(f'user-agent={userAgent}')
-
-        # Initialize the WebDriver
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
         driver.get(url)  # Load the page
 
         # Log in to the first page
@@ -74,13 +73,13 @@ def scrape(username: str, password: str, lists: [str]) -> str:
 
         #print("Second Page Loaded.")
 
-        WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[@aria-label='close banner']"))).click()
+        WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[@aria-label='close banner']"))).click()  # Close banner
 
-        buttonList = driver.find_elements(By.ID, "closeModalBtn")
+        close_button_list = driver.find_elements(By.ID, "closeModalBtn")  # Find button to close survey pop up
 
         # Click two close buttons
-        for button in buttonList:
-            driver.execute_script("arguments[0].click();", button) # Force click using JavaScript (idk why)
+        for close_button in close_button_list:
+            driver.execute_script("arguments[0].click();", close_button) # Force click using JavaScript (idk why but it works)
 
         #print("Closed Surveys.")
 
@@ -90,61 +89,80 @@ def scrape(username: str, password: str, lists: [str]) -> str:
         # Scrape the webpage
 
         # Once page is loaded, find the list parent div
-        element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'side-panel__content screen-shell-aside__content')]")))
+        list_parent_element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'side-panel__content screen-shell-aside__content')]")))
 
         # Get all anchor links
-        webEleList = element.find_elements(By.TAG_NAME, "a")
+        anchor_element_list = list_parent_element.find_elements(By.TAG_NAME, "a")
 
-        # Create a new list to hold the filtered elements
-        filteredList = []
+        # Scrape selected lists if provided
+        if lists:
+            # Create a new list to hold the filtered elements
+            filtered_anchor_list = []
 
-        # Narrow list to specified names
-        for ele in webEleList:
-            if ele.text in lists:  # Assuming 'lists' is a list or set of valid names
-                filteredList.append(ele)
+            # Narrow list to specified names
+            for anchor_element in anchor_element_list:
+                if anchor_element.text in lists:  # Assuming 'lists' is a list or set of valid names
+                    filtered_anchor_list.append(anchor_element)
 
-        # If you want to update 'webEleList' to only include the specified names
-        webEleList = filteredList
+            anchor_element_list = filtered_anchor_list
 
-        # Loop over each list
-        for webElement in webEleList:
-            #print("List Title: ", webElement.text)  # Prints list title
+        anchor_element = anchor_element_list[0]  # Used in loop
 
-            parentElem = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.CLASS_NAME, "table__body")))
+        num_loops = len(anchor_element_list)
 
-            webElement.click()  # Clicks on list
+        # Loop over each list using len because elements must be refreshed each iteration
+        for i in range(num_loops):
+            WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(anchor_element)).click()  # Clicks on list
+
+            # Wait for blurry cover to go away
+            WebDriverWait(driver, timeout).until(
+                EC.invisibility_of_element_located((By.XPATH, "//*[contains(@class, 'm-9814e45f mantine-Overlay-root')]")))
+            #print("Overlay is gone")
+            # could try reloading page here if overlay doesnt go bye bye
+
+            #print("List Title: ", anchor_element.text)  # Prints list title
+
+            athlete_parent_element = WebDriverWait(driver, timeout).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "table__body")))
 
             # Get list of all athlete names
-            athleteList = parentElem.find_elements(By.TAG_NAME, "tr")
+            WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.TAG_NAME, "tr")))
+            athlete_element_list = athlete_parent_element.find_elements(By.TAG_NAME, "tr")
+
+            #print(len(athlete_element_list))
 
             # Loop over each athlete
-            for athlete in athleteList:
-                WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(athlete)).click()
+            for athlete_element in athlete_element_list:
+                WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(athlete_element)).click()
 
-                nameElem = WebDriverWait(athlete, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'focus-ring--active athlete-profile-contact__header--title')]")))
+                name_element = WebDriverWait(athlete_element, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'focus-ring--active athlete-profile-contact__header--title')]")))
 
-                numberElem = athlete.find_element(By.XPATH, "//*[contains(@aria-label, 'Phone')]")
+                number_element = athlete_element.find_element(By.XPATH, "//*[contains(@aria-label, 'Phone')]")
 
                 try:
-                    addressElem = athlete.find_element(By.XPATH, "//*[contains(@aria-label, 'Address')]")
+                    address_element = athlete_element.find_element(By.XPATH, "//*[contains(@aria-label, 'Address')]")
                     entry = {
-                        'Name': nameElem.text,
-                        'Number': numberElem.text,
-                        'Address': addressElem.text
+                        'Name': name_element.text,
+                        'Number': number_element.text,
+                        'Address': address_element.text
                     }
                 except NoSuchElementException:
                     entry = {
-                        'Name': nameElem.text,
-                        'Number': numberElem.text,
+                        'Name': name_element.text,
+                        'Number': number_element.text,
                         'Address': 'None'
                     }
 
                 # Append data to list
                 data.append(entry)
 
-                driver.find_element(By.XPATH, "//*[@aria-label='Close Athlete Profile']").click()
+                driver.find_element(By.XPATH, "//*[@aria-label='Close Athlete Profile']").click()  # Close athlete profile
 
-        # Create a DataFrame from the list of dictionaries
+            anchor_element_list.pop(0)  # Get rid of looped over list
+            if anchor_element_list:
+                anchor_element = anchor_element_list[0]  # Update to next list
+
+        # Create a DataFrame from the list of dictionaries to simplify excelification
         df = pd.DataFrame(data)
 
         timestamp = datetime.datetime.now().strftime("%Y_%m_%d")
@@ -154,7 +172,11 @@ def scrape(username: str, password: str, lists: [str]) -> str:
         # Write DataFrame to an Excel file
         df.to_excel(excel_file_path, index=False, engine='openpyxl')
 
+        driver.quit()  # End session
+
         return 'success'
 
-    except:
+    except Exception as e:
+        print(e)
+        driver.quit()
         return 'failure'
